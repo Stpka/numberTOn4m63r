d1031473f4190ea08fe0c5bb64ccbd46b80e3fa6
Stpka <miholapstepan@gmail.com>
2021-09-19 16:22:07 +0300

    Add convert word-number and number-word for 0-5

--- numberTOn4m63r.py
+++ numberTOn4m63r.py
@@ -1,2 +1,16 @@
- #test
- 
+words 	= ['zero', 'one', 'two', 'free', 'four', 'five']
+
+
+
+def conntow(number): # convert number to word
+	print(words[number])
+
+
+def conwton(word): #convert word to number
+	for i in range(6):
+		if word == words[i]:
+			print(i)
+			break
+
+		else:	i+=1
+
